7a40c1d5093fa6ce11ba545433dc02f0bab1c1ea
bschwartz10 <bschwartz92@gmail.com>
2017-04-10 22:38:37 -0600

    complete slug integration

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,11 +1,7 @@
 class CategoriesController < ApplicationController
 
   def show
-    @category = Category.find_by_slug(params[:id])
-    respond_to do |format|
-      format.html # show.html.erb
-      format.json { render json: @category }
-    end
+    @category = Category.find_by_slug(params[:category])
   end
 
 end
