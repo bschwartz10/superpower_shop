4a40e6795fccc6ef0a9d4272a877303293c32555
bschwartz10 <bschwartz92@gmail.com>
2017-04-12 19:49:59 -0600

    uncomment database cleaner in seed.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
-# require 'database_cleaner'
-#
-# DatabaseCleaner.strategy = :truncation
-# DatabaseCleaner.clean
+require 'database_cleaner'
+
+DatabaseCleaner.strategy = :truncation
+DatabaseCleaner.clean
 
 categories = ["street level", "planetary", "cosmic"]
 urls = ["http://www.pngall.com/wp-content/uploads/2017/03/Peter-Pan-Free-Download-PNG.png", "http://www.toonsup.com/users/j/java/time_travel_100417_1424.jpg", "https://sparketh.com/wp-content/uploads/2015/08/Kid-super-power-881x588.jpg", "http://blog.scribd.com/wp-content/uploads/2016/03/superhero.png", "http://quizsocial.com/quimg/superpower/results/2.jpg","http://usa.chinadaily.com.cn/weekly/img/attachement/jpg/site181/20120406/00221917e13e10e8845b3e.jpg"]
