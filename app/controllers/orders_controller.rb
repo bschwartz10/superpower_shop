6fbe19e1a080037eb2b3a72bb1a7747ca6012e7e
bschwartz10 <bschwartz92@gmail.com>
2017-04-13 18:15:59 -0600

    can save order

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,16 +1,25 @@
 class OrdersController < ApplicationController
   include ActionView::Helpers::TextHelper
+
+  def index
+    @orders = Order.all
+  end
+
   def create
-    order = Order.new(user_id: User.find(params[:user_id]))
+    order = Order.new(user_id: User.find(params[:user_id].to_i).id)
     @briefcase.contents.each do |power_id, quantity|
-      order.powers.new(power_id: power_id, quantity: quantity)
+      order.powers << Power.find(power_id)
     end
-    
+
     if order.save
       session[:briefcase] = nil
-      flash[:notice] = "Your order are saved! You saved #{briefcase.powers.count} powers."
-      redirect_to powers_path
+      flash[:notice] = "Your order was saved! You saved #{@briefcase.powers.count} powers."
+      redirect_to order_path(order)
     else
     end
+  end
+
+  def show
+
   end
 end
