46a444047c9aac2907aeed8febbbf03a48891be5
VictoriaVasys <vvasys@gmail.com>
2017-04-14 08:28:49 -0600

    fiddle with buttons and alignment

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,17 +2,16 @@ module ApplicationHelper
 
    def decrease_quantity_button(power)
       if @briefcase.contents[power.id.to_s] > 1
-       button_to '-', briefcase_path(power_id: power.id, type: "decrease"), class: "btn btn-info", method: :patch
+        button_to '-', briefcase_path(power_id: power.id, type: "decrease"), class: "btn btn-info decrease", method: :patch
       end
    end
 
    def checkout_button_or_sign_up
-      if current_user
-         link_to 'Checkout Abilities', class: 'btn btn-primary'
+     if current_user
+        link_to 'Checkout Abilities', class: 'btn btn-primary'
       elsif session[:user_id].nil?
-         link_to "Login or Create Account to Buy Your Abilities!", login_path, class: 'btn btn-info'
-      end
+        link_to "Login or Create Account to Buy Your Abilities!", login_path, class: 'btn btn-info'
+     end
    end
-
 
 end
