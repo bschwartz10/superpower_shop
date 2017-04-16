651acee3943ed11526d229cec3e9e92412fcef87
bschwartz10 <bschwartz92@gmail.com>
2017-04-16 17:27:56 -0600

    add conditional for login home page

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -2,25 +2,25 @@ class Admin::OrdersController < Admin::BaseController
 
   def dashboard
     @orders = Order.all
-    @ordered_orders_count = Order.where(status: "Ordered").pluck(:status).count
-    @paid_orders_count = Order.where(status: "Paid").pluck(:status).count
-    @cancelled_orders_count = Order.where(status: "Cancelled").pluck(:status).count
-    @completed_orders_count = Order.where(status: "Completed").pluck(:status).count
+    @ordered_orders_count = Order.where(status: "ordered").pluck(:status).count
+    @paid_orders_count = Order.where(status: "paid").pluck(:status).count
+    @cancelled_orders_count = Order.where(status: "cancelled").pluck(:status).count
+    @completed_orders_count = Order.where(status: "completed").pluck(:status).count
   end
 
   def filter
     type = params[:type]
       if type == "ordered"
-        @orders = Order.where(status: "Ordered")
+        @orders = Order.where(status: "ordered")
         render :ordered
       elsif type == "paid"
-        @orders = Order.where(status: "Paid")
+        @orders = Order.where(status: "paid")
         render :paid
       elsif type == "cancelled"
-        @orders = Order.where(status: "Cancelled")
+        @orders = Order.where(status: "cancelled")
         render :cancelled
       elsif type == "completed"
-        @orders = Order.where(status: "Completed")
+        @orders = Order.where(status: "completed")
         render :completed
       end
   end
