9c8e958e2ac59643c08a11bf4a3c33222d5bb53c
bschwartz10 <bschwartz92@gmail.com>
2017-04-16 14:32:12 -0600

    ordered orders count method implemented

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -2,6 +2,8 @@ class Admin::OrdersController < Admin::BaseController
 
   def dashboard
     @orders = Order.all
+    @ordered_orders_count = Order.where(status: "ordered").pluck(:status).count
+
   end
 
 end
