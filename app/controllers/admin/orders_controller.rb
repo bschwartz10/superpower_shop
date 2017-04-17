b2c765b3d6308d041920137ebefe1882b76b47d8
bschwartz10 <bschwartz92@gmail.com>
2017-04-17 11:14:42 -0600

    fix order statuses

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -2,25 +2,25 @@ class Admin::OrdersController < Admin::BaseController
 
   def dashboard
     @orders = Order.all
-    @ordered_orders_count = Order.where(status: "ordered").pluck(:status).count
-    @paid_orders_count = Order.where(status: "paid").pluck(:status).count
-    @cancelled_orders_count = Order.where(status: "cancelled").pluck(:status).count
-    @completed_orders_count = Order.where(status: "completed").pluck(:status).count
+    @ordered_orders_count = Order.where(status: "Ordered").pluck(:status).count
+    @paid_orders_count = Order.where(status: "Paid").pluck(:status).count
+    @cancelled_orders_count = Order.where(status: "Cancelled").pluck(:status).count
+    @completed_orders_count = Order.where(status: "Completed").pluck(:status).count
   end
 
   def filter
     type = params[:type]
       if type == "ordered"
-        @orders = Order.where(status: "ordered")
+        @orders = Order.where(status: "Ordered")
         render :ordered
       elsif type == "paid"
-        @orders = Order.where(status: "paid")
+        @orders = Order.where(status: "Paid")
         render :paid
       elsif type == "cancelled"
-        @orders = Order.where(status: "cancelled")
+        @orders = Order.where(status: "Cancelled")
         render :cancelled
       elsif type == "completed"
-        @orders = Order.where(status: "completed")
+        @orders = Order.where(status: "Completed")
         render :completed
       end
   end
