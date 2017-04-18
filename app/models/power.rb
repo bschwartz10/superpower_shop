6ffc7e81688d3c01347f8f37951ad3ca762bc345
bschwartz10 <bschwartz92@gmail.com>
2017-04-18 17:46:56 -0600

    add default image for paperclip

--- app/models/power.rb
+++ app/models/power.rb
@@ -5,6 +5,7 @@ class Power < ActiveRecord::Base
   belongs_to :category
   has_many :order_powers
   has_many :orders, through: :order_powers
-  has_attached_file :image
+  has_attached_file :image, styles: {medium: "560x315", thumb: "304x171"},
+    default_url: "/images/default.jpg"
   validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png"]
 end
